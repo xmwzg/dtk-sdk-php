ce142121e8c7722f7ae6dddbe6315588aa44a4f1
wzg <2607010328@qq.com>
2021-03-15 10:23:07 +0800

    update

--- src/DTK/DingTalk.php
+++ src/DTK/DingTalk.php
@@ -17,18 +17,36 @@ class DingTalk
      * 获取配置参数
      * @Author   xmwzg
      * @DateTime 2021-03-08
-     * @param    {system} 指定系统参数 case 案例库 crm 海豚系统
      * @return   config 
      */
-    public function getConfig($system='case')
+    public function getConfig()
     {
-        $params = \Yii::$app->params[$system];
+        $params = \Yii::$app->params['dingtalk'];
+
         $this->login_appid = $params['login_appid'];
         $this->login_appsecret = $params['login_appsecret'];
         $this->appkey = $params['appkey'];
         $this->appsecret = $params['appsecret'];
+        $this->agentid = $params['agentId'];
     }
+    /**
+     * 获取token
+     * @Author   xmwzg
+     * @DateTime 2021-03-12
+     * @param    {string}
+     * @return   [type]     [description]
+     */
+    public function getToken(){
+        $this->getConfig();
+        //token
+        $c_get = new \DingTalkClient(\DingTalkConstant::$CALL_TYPE_OAPI, \DingTalkConstant::$METHOD_GET , \DingTalkConstant::$FORMAT_JSON);
+        $req = new \OapiGettokenRequest;
+        $req->setAppkey($this->appkey);
+        $req->setAppsecret($this->appsecret);
+        $token = $c_get->execute($req, '', "https://oapi.dingtalk.com/gettoken");
 
+        return $token;
+    }
     /**
      * 钉钉扫码登陆
      * @Author   xmwzg
@@ -36,8 +54,8 @@ class DingTalk
      * @param    {code} 临时code
      * @return   [type]
      */
-    public function dingLogin($system,$code){
-        $this->getConfig($system);
+    public function dingLogin($code){
+        $this->getConfig();
         $c_post = new \DingTalkClient(\DingTalkConstant::$CALL_TYPE_OAPI, \DingTalkConstant::$METHOD_POST , \DingTalkConstant::$FORMAT_JSON);
         $req = new \OapiSnsGetuserinfoBycodeRequest;
         $req->setTmpAuthCode($code);
@@ -47,14 +65,7 @@ class DingTalk
         }
 
         //token
-        $c_get = new \DingTalkClient(\DingTalkConstant::$CALL_TYPE_OAPI, \DingTalkConstant::$METHOD_GET , \DingTalkConstant::$FORMAT_JSON);
-        $req = new \OapiGettokenRequest;
-        $req->setAppkey($this->appkey);
-        $req->setAppsecret($this->appsecret);
-        $token = $c_get->execute($req, '', "https://oapi.dingtalk.com/gettoken");
-        if($token->errcode !== 0){
-          return $token;
-        }
+        $token = $this->getToken();
 
         //获取userID
         $req = new \OapiUserGetbyunionidRequest;
@@ -78,15 +89,66 @@ class DingTalk
         }
         return $userinfo;
     }
+
     /**
-     * 获取员工花名册信息
+     * 根据手机号获取用户信息
+     * @Author   xmwzg
+     * @DateTime 2021-03-12
+     * @param    {string}
+     * @return   [type]     [description]
+     */
+    public function getUserId($mobile){
+
+        $token = $this->getToken();
+
+        $c = new \DingTalkClient(\DingTalkConstant::$CALL_TYPE_OAPI, \DingTalkConstant::$METHOD_POST , \DingTalkConstant::$FORMAT_JSON);
+        $req = new \OapiV2UserGetbymobileRequest;
+        $req->setMobile($mobile);
+        $resp = $c->execute($req, $token->access_token, "https://oapi.dingtalk.com/topapi/v2/user/getbymobile");
+
+        if($resp->errcode == 0){
+            return $resp->result->userid;
+        }
+
+        return '091939145829210195';
+
+    }
+    /**
+     * 钉钉发送普通消息
      * @Author   xmwzg
      * @DateTime 2021-03-09
      * @param    {string}
      * @return   [type]
      */
-    public function hmcInfo($userid){
-      echo $userid;
+    public function sendMessage($send_name,$mobile='13522325326',$worker_id){
+
+      $this->getConfig();
+      $token = $this->getToken();
+      $url = 'http://crm.ret.cn/';
+      $notice = '';
+      if(empty($mobile)){
+        $mobile = '13522325326';
+        $notice = $worker_id.'电话号码为空';
+      }
+      $userid = $this->getUserId($mobile);
+      if (!YII_ENV_PROD){
+          $userid = '091939145829210195';
+      }
+      $c = new \DingTalkClient(\DingTalkConstant::$CALL_TYPE_OAPI, \DingTalkConstant::$METHOD_POST , \DingTalkConstant::$FORMAT_JSON);
+      $req = new \OapiMessageCorpconversationAsyncsendV2Request;
+      $req->setAgentId($this->agentid);
+      $req->setUseridList($userid);
+      $req->setToAllUser("false");
+      $msg = new \Msg;
+      $msg->msgtype="link";
+      $link = new \Link;
+      $link->picUrl="https://ret-crm.oss-cn-beijing.aliyuncs.com/logo/dinglink.png";
+      $link->messageUrl="dingtalk://dingtalkclient/page/link?url=".urlencode($url)."&pc_slide=false";
+      $link->title="海豚提醒".$notice;
+      $link->text="海豚提醒您，".$send_name."给您转介了一个项目，请及时跟进处理。";
+      $msg->link = $link;
+      $req->setMsg($msg);
+      $resp = $c->execute($req, $token->access_token, "https://oapi.dingtalk.com/topapi/message/corpconversation/asyncsend_v2");
     }
 
 }
